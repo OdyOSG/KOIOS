fd07246fa4b9572aa5cdc7437c6f2b9294a2f4bd
laurencelawrencearcher <laurence.lawrence@odysseusinc.com>
2023-08-09 11:53:38 +0200

    Update auto mode

--- R/automatic.R
+++ R/automatic.R
@@ -5,22 +5,35 @@
 findReference <- function(vcf){
   vcf.df <- processVCF(vcf)
 
-  #Sample 100 rows
-  if(dim(vcf.df)[1] > 100){
-    vcf.df <- vcf.df[sample(x = dim(vcf.df)[1], size = 100, replace = F),]
+  vcf.snv <- vcf.df[stringr::str_length(vcf.df$REF) == 1 & stringr::str_length(vcf.df$ALT) == 1,]
+
+  if(dim(vcf.snv)[1] < 15){
+    message("Less than 15 SNVs detected. Sampling indels.")
+    vcf.snv <- vcf.df
+    if(dim(vcf.snv)[1] > 50){
+      vcf.snv <- vcf.snv[sample(x = dim(vcf.snv)[1], size = 50, replace = F),]
+    } else{
+      message("Less than 50 variants detected. Manual reference check reccomended.")
+    }
+  } else {
+    vcf.snv <- vcf.snv[sample(x = dim(vcf.snv)[1], size = 15, replace = F),]
   }
 
   ref.hg18 <- loadReference("hg18")
   ref.hg19 <- loadReference("hg19")
   ref.hg38 <- loadReference("hg38")
 
-  vcf.hg18 <- generateHGVSG(vcf.df = vcf.df, ref.df = ref.hg18)
-  vcf.hg19 <- generateHGVSG(vcf.df = vcf.df, ref.df = ref.hg19)
-  vcf.hg38 <- generateHGVSG(vcf.df = vcf.df, ref.df = ref.hg38)
+  vcf.hg18 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg18)
+  vcf.hg19 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg19)
+  vcf.hg38 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg38)
 
   alleles.hg18 <- processClinGen(vcf.df = vcf.hg18, ref = "hg18", generateAll = F)
   alleles.hg19 <- processClinGen(vcf.df = vcf.hg19, ref = "hg19", generateAll = F)
   alleles.hg38 <- processClinGen(vcf.df = vcf.hg38, ref = "hg38", generateAll = F)
+
+  alleles.hg18 <- alleles.hg18[!is.na(alleles.hg18$hgvsg),]
+  alleles.hg19 <- alleles.hg19[!is.na(alleles.hg19$hgvsg),]
+  alleles.hg38 <- alleles.hg38[!is.na(alleles.hg38$hgvsg),]
 
   #Find reference genome
   resultLength <- c(dim(alleles.hg18)[1],dim(alleles.hg19)[1],dim(alleles.hg38)[1])
--- userScript.R
+++ userScript.R
@@ -6,7 +6,7 @@ library(KOIOS)
 concepts <- loadConcepts()
 
 #Input VCF
-vcf <- loadVCF(userVCF = "Test VCF/GSE142442_RAW/GSE142442_12_AST.vcf")
+vcf <- loadVCF(userVCF = "Test VCF/GSE142442_RAW/GSE142442_129_MWS.vcf")
 
 #Please enter one of hg18, hg19, hg38 or "auto"
 ref <- "auto"
@@ -31,3 +31,43 @@ alleles.df <- processClinGen(vcf.df,ref,generateAll = generateTranscripts)
 
 concepts.df <- addConcepts(alleles.df, concepts)
 
+
+
+
+
+
+vcf.df <- processVCF(vcf)
+
+vcf.snv <- vcf.df[stringr::str_length(vcf.df$REF) == 1 & stringr::str_length(vcf.df$ALT) == 1,]
+
+if(dim(vcf.snv)[1] < 10){
+  message("Less than 10 SNVs detected.")
+  vcf.snv <- vcf.df
+}
+
+#Sample 100 rows
+if(dim(vcf.snv)[1] > 15){
+  vcf.snv <- vcf.snv[sample(x = dim(vcf.snv)[1], size = 15, replace = F),]
+}
+
+ref.hg18 <- loadReference("hg18")
+ref.hg19 <- loadReference("hg19")
+ref.hg38 <- loadReference("hg38")
+
+vcf.hg18 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg18)
+vcf.hg19 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg19)
+vcf.hg38 <- generateHGVSG(vcf.df = vcf.snv, ref.df = ref.hg38)
+
+alleles.hg18 <- processClinGen(vcf.df = vcf.hg18, ref = "hg18", generateAll = F)
+alleles.hg19 <- processClinGen(vcf.df = vcf.hg19, ref = "hg19", generateAll = F)
+alleles.hg38 <- processClinGen(vcf.df = vcf.hg38, ref = "hg38", generateAll = F)
+
+alleles.hg18 <- alleles.hg18[!is.na(alleles.hg18$hgvsg),]
+alleles.hg19 <- alleles.hg19[!is.na(alleles.hg19$hgvsg),]
+alleles.hg38 <- alleles.hg38[!is.na(alleles.hg38$hgvsg),]
+
+#Find reference genome
+resultLength <- c(dim(alleles.hg18)[1],dim(alleles.hg19)[1],dim(alleles.hg38)[1])
+
+#Set reference genome
+ref <- c("hg18","hg19","hg38")[which.max(resultLength)]
